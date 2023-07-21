a022b85af50bdb27f7b32febc2c88cccc04a5c5d
Dhruvil <fiesta357@gmail.com>
2023-07-21 14:30:06 +0530

    Added Vision AI API

--- Server/main.py
+++ Server/main.py
@@ -1,4 +1,4 @@
-
+from google.cloud import vision
 from flask import render_template
 from distutils.log import debug
 from fileinput import filename
@@ -17,9 +17,40 @@ def success():
     if request.method == 'POST':  
         f = request.files['file']
         f.save(f.filename)
-        fileToWork = f
+        detect_properties(f.filename)
         return render_template("Acknowledgement.html", name = f.filename) 
           
+          
+def detect_properties(path):
+    """Detects image properties in the file."""
+    
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
+    print("Properties:")
+
+    for color in props.dominant_colors.colors:
+        print(f"fraction: {color.pixel_fraction}")
+        print(f"\tr: {color.color.red}")
+        print(f"\tg: {color.color.green}")
+        print(f"\tb: {color.color.blue}")
+        print(f"\ta: {color.color.alpha}")
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+        
+        
+
 # run the application
 if __name__ == "__main__":
     app.run(debug=True)
