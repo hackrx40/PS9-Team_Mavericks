e49a1ddc948b99d7bc1d218a69f2446db4b98a4b
Vedant Singh <vs2309@srmist.edu.in>
2023-07-21 21:38:25 +0530

    changing python server to match the node server

--- NodejsUI/server.js
+++ NodejsUI/server.js
@@ -16,65 +16,20 @@ const isFileValid = (file) => {
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.post('/checks', async (req, res, next) => {
+app.post('/checks', function (req, res){
     const form = new formidable.IncomingForm();
-    const uploadFolder = path.join(__dirname, "public", "files");
-    form.multiples = true;
-    form.parse(req, async (err, fields, files) => {
-        console.log(fields);
-        console.log(files);
-        if (err) {
-          console.log("Error parsing the files");
-          return res.status(400).json({
-            status: "Fail",
-            message: "There was an error parsing the files",
-            error: err,
-          });
-        }
-      });
-      if (!files.myFile.length) {
-        //Single file
-      
-        const file = files.myFile;
-      
-        // checks if the file is valid
-        const isValid = isFileValid(file);
-      
-        // creates a valid name by removing spaces
-        const fileName = encodeURIComponent(file.name.replace(/\s/g, "-"));
-      
-        if (!isValid) {
-          // throes error if file isn't valid
-          return res.status(400).json({
-            status: "Fail",
-            message: "The file type is not a valid type",
-          });
-        }
-        try {
-          // renames the file in the directory
-          fs.renameSync(file.path, join(uploadFolder, fileName));
-        } catch (error) {
-          console.log(error);
-        }
-      
-        try {
-          // stores the fileName in the database
-          const newFile = await File.create({
-            name: `files/${fileName}`,
-          });
-          return res.status(200).json({
-            status: "success",
-            message: "File created successfully!!",
-          });
-        } catch (error) {
-          res.json({
-            error,
-          });
-        }
-      } else {
-        // TODO:Multiple files
-        console.log("multiple files")
-      }
+    form.parse(req, function (err, fields, files) {
+ 
+        let oldPath = files.profilePic.filepath;
+        let newPath = path.join(__dirname, 'uploads')
+            + '/' + files.profilePic.name
+        let rawData = fs.readFileSync(oldPath)
+ 
+        fs.writeFile(newPath, rawData, function (err) {
+            if (err) console.log(err)
+            return res.send("Successfully uploaded -> ", newPath)
+        })
+    })
 });
 
 app.listen(port, () => {
