a0694a40b09785663b9ee85f42ed3ff275a8aa92
Vedant Singh <vs2309@srmist.edu.in>
2023-07-21 11:19:42 +0530

    init commit

---
+++ FrontEnd/App/sus_alert/README.md
@@ -0,0 +1,3 @@
+# sus_alert
+
+A new Flutter project.
---
+++ FrontEnd/App/sus_alert/android/app/src/main/kotlin/com/example/sus_alert/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sus_alert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
