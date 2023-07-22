77b449a39a9531bd36c2543cdee69dac1b7c5e33
Teja <teja@gmail.com>
2023-07-22 09:34:10 +0530

    Added handwriting analyser

--- py-server/server.py
+++ py-server/server.py
@@ -67,10 +67,23 @@ def checkStamp(filename):
     image = tf.expand_dims(image, axis=0)
     results = model.predict(image)
     if results[0][0] < 1:
-        result = "Tampering Detected in stamp, further investigation required"
+        result = "Stamp Detected"
         return result
     else:
-        return "Passed"
+        return "Stamp not detected"
+             
+def checkHand(filename):
+    model = tf.keras.models.load_model('./ml-handwriting/keras_model.h5')   
+    image = cv2.imread(filename)
+    image = cv2.resize(image, (224, 224))
+    image = image.astype('float32') / 255.0
+    image = tf.expand_dims(image, axis=0)
+    results = model.predict(image)
+    if results[0][0] < 1:
+        result = "No handwriting found"
+        return result
+    else:
+        return "Handwriting found!!"
 
 
 
@@ -92,21 +105,23 @@ def runTests(data):
     print('received message: ' + str(data))
     print(open("filename.txt", "r").read())
     socketio.emit('status', {
-        "current":0,
         "done":1,
         "response": checkModificationinPDF(open("filename.txt", "r").read()),
     })
     
     socketio.emit('status', {
-        "current":1,
         "done":2,
         "response": checkSign(open("filename.txt", "r").read()),
     })
     
     socketio.emit('status', {
-        "current":2,
         "done":3,
         "response": checkStamp(open("filename.txt", "r").read()),
+    })
+    
+    socketio.emit('status', {
+        "done":4,
+        "response": checkHand(open("filename.txt", "r").read()),
     })
     
     
